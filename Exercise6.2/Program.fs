8974f2687da46713ee41132edbb15951927ef84c
Sara Lopez <slopezalaguero@gmail.com>
2019-03-17 14:20:26 +0100

    Exercise 6.2

---
+++ Exercise6.2/Program.fs
@@ -0,0 +1,13 @@
+﻿
+let rec pclCollect (ls:'a list) = 
+    let ls = List.rev ls
+
+    let rec appendToFinal (sub_list:'a list) previous (final_list: 'a list list) (ls:char list) =
+       match ls with
+       | [] -> (sub_list :: final_list)
+       | hd :: tail -> if hd = previous then appendToFinal (hd :: sub_list) hd final_list tail
+                       else appendToFinal ([hd]) hd (sub_list :: final_list) tail
+
+    appendToFinal [] ls.[0] [] ls
+    
+pclCollect ['p'; 'p'; 's'; 'c'; 'a'; 'l';'a';'p'; 'c';'l';'y'];;       
